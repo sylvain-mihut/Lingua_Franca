3d30d23c7cc6f5e17eeec49a62459ab3ede1c344
sylvain-mihut <sylvain.mihut@laplateforme.io>
2023-06-13 11:58:19 +0200

    program functional, css comming soon

--- main.py
+++ main.py
@@ -4,17 +4,48 @@ import googletrans
 
 
 app = Flask(__name__, template_folder="template")
+translator = Translator()  # Définit les langues source et cible
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    translator = Translator()  # Définit les langues source et cible
-    text = "Bonjour, comment ça va ?"  # Texte à traduire
-    translation = translator.translate(text, dest='en')  # Traduit le texte
-    print(translation.text)  # Affiche la traduction
-    lang = googletrans.LANGUAGES.values()
-    print(lang)
-    return render_template('index.html', data=lang)
+    lang = list(googletrans.LANGUAGES.values())
+    if request.method == 'POST':
+        input_data = request.form.get('input')
+        src_input_lang = request.form.get('src_input_lang')
+        src_output_lang = request.form.get('src_output_lang')
+
+        
+
+
+        if src_input_lang != None and src_output_lang != None and input_data != None:
+            translation = translator.translate(input_data, src=src_input_lang, dest=src_output_lang)  # Traduit le texte
+        return render_template('index.html',  traduction=translation.text, data=lang)
+    
+    else :
+        return render_template('index.html', data=lang)
+
+
+
+
+
+
+
+
+
+    # print(src_output_lang)
+    # print(input_data)
+    # print(str(src_input_lang))
+
+        
+        
+        # if src_input_lang != None and src_output_lang != None and input_data != None: 
+        #     translation = translator.translate(input_data, src=src_input_lang, dest=src_output_lang)  # Traduit le texte
+        #     print(translation.text)  # Affiche la traduction
+
+        #     return render_template('index.html',  traduction=translation.text)
+
+        # return render_template('index.html', data=lang)
 
 if __name__ == "__main__":
     app.run(debug=True)
